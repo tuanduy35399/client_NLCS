10ef71f65437160c74d50745b59a06b648e5e877
Duy <gochoctapcuatuanduy@gmail.com>
2026-05-28 20:27:16 +0700

    init and create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import { Header, Footer } from "../components/Template";
+//Từ thư viện zod
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [score, setScore] = useState("");
+  const [errMs, setErrMs] = useState("");
+  const handleInput = (e) => {
+    const value = e.target.value;
+    setScore(value);
+    const numericScore = parseFloat(value);
+    if (value === "") {
+      setErrMs(""); //chuoi rong la nguoi dung chua nhap gi => khong co loi
+    } else if (isNaN(numericScore) || numericScore > 10 || numericScore < 0) {
+      setErrMs("Số không được vượt quá phạm vi từ 0 đến 10");
+    } else {
+      setErrMs(""); //diem hop le thi xoa thong bao loi
+    }
+  };
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      {isLoading ? (
+        <div className="flex justify-center items-center min-h-screen w-full">
+          <span className="loading loading-spinner size-16"></span>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      ) : (
+        <>
+          <Header />
+          <div className="w-full min-h-screen flex justify-center items-center">
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-3xl border p-4 ">
+              <p className="text-2xl font-medium mb-3 text-center">
+                Nhập điểm thi THPT của bạn
+              </p>
+              <div className="flex flex-row flex-warp justify-center items-start gap-5">
+                <div className="w-auto">
+                  <label className="label">Toán học</label>
+                  <input
+                    type="number"
+                    step="0.01" //cho phep nhap so thap phan 2 chu so
+                    className="input validator"
+                    required
+                    placeholder="Nhập số từ 0 to 10"
+                    min="0"
+                    max="10"
+                    value={score}
+                    onChange={handleInput}
+                  />
+                  <p className="validator-hint">{errMs}</p>
+                  <label className="label">Ngữ văn</label>
+                  <input
+                    type="number"
+                    step="0.01" //cho phep nhap so thap phan 2 chu so
+                    className="input validator"
+                    required
+                    placeholder="Nhập số từ 0 to 10"
+                    min="0"
+                    max="10"
+                    value={score}
+                    onChange={handleInput}
+                  />
+                  <p className="validator-hint">{errMs}</p>
+                </div>
+                <div className="w-80">
+                  <label className="label">Môn tự chọn 1</label>
+                  <div className="flex flex-row gap-2">
+                    <input
+                      type="email"
+                      className="input"
+                      placeholder="Ngữ Văn"
+                    />
+                    <input
+                      type="number"
+                      step="0.01" //cho phep nhap so thap phan 2 chu so
+                      className="input validator"
+                      required
+                      placeholder="Nhập số từ 0 to 10"
+                      min="0"
+                      max="10"
+                      value={score}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <p className="validator-hint">{errMs}</p>
+                  <label className="label">Môn tự chọn 2</label>
+                  <div className="flex flex-row gap-2">
+                    <input
+                      type="email"
+                      className="input"
+                      placeholder="Ngữ Văn"
+                    />
+                    <input
+                      type="number"
+                      step="0.01" //cho phep nhap so thap phan 2 chu so
+                      className="input validator"
+                      required
+                      placeholder="Nhập số từ 0 to 10"
+                      min="0"
+                      max="10"
+                      value={score}
+                      onChange={handleInput}
+                    />
+                  </div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+                  <p className="validator-hint">{errMs}</p>
+                </div>
+              </div>
+            </fieldset>
+          </div>
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
